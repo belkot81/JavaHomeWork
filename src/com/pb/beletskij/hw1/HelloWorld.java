7abdfcd5ea3d17c469d2f66ba004d167d5acd89c
Konstantin Beletskij <Konstantin.Beletskij@privatbank.ua>
2021-10-10 22:36:00 +0300

    init commit

---
+++ src/com/pb/beletskij/hw1/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.pb.beletskij.hw1;
+
+import java.sql.SQLOutput;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
